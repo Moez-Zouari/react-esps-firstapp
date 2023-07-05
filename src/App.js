7737f1b441a0b0aa204b4d91bd023a658c9b9410
moezzouari <moez@octoplus.tech>
2023-07-05 14:08:39 +0100

    Add Class Article

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
+
 import Client from './Client';
+import Article from './Article';
 
 function App() {
   return (
     <div className="App">
       <h1>Bienvenu Dans Notre Site</h1>
-      <Client></Client>
+      <Client />
+      <Article />
     </div>
   );
 }
---
+++ src/Article.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Article extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Liste Des Articles</h1>
+                <h2>Article Soldés</h2>
+            </div>
+        )
+    }
+}
+
+export default Article
