3f47e7ba1b2856f6527599a82ca2e971703ddbd4
moezzouari <moez@octoplus.tech>
2023-07-05 14:19:24 +0100

    Adding State Client Class

--- src/Client.jsx
+++ src/Client.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 
 class Client extends Component {
-
+    constructor() {
+        super()
+        this.state = {
+            nom: "ESPS",
+            adresse: "Sfax",
+            tel: "21606505"
+        }
+    }
     render() {
         return (
-            <div>Bonjour Monsieur</div>
+            <>
+                <h2>Le nom de la société est : {this.state.nom}</h2>
+                <h2>L'adresse de la société est : {this.state.adresse} contact : {this.state.tel}</h2>
+                <div>Bonjour Monsieur</div>
+            </>
         )
     }
 }
